e096882222d2c6a28b8ceefffe0c13f8e9f613cd
hynrusang <mashmalo1871@gmail.com>
2022-11-17 06:46:20 +0900

    0.1.1
    -login 기능 구현 (테스트 단계)

--- app/src/main/java/com/example/pbl/LoginActivity.kt
+++ app/src/main/java/com/example/pbl/LoginActivity.kt
@@ -3,9 +3,14 @@ package com.example.pbl
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class LoginActivity : AppCompatActivity() {
+    lateinit var idfield : TextView
+    lateinit var passfield : TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.log_in)
@@ -15,8 +20,17 @@ class LoginActivity : AppCompatActivity() {
             startActivity(intent)
         }
         findViewById<Button>(R.id.login).setOnClickListener {
-            val intent = Intent(this, UserInfoActivity::class.java)
-            startActivity(intent)
+            idfield = findViewById<TextView>(R.id.id_field)
+            passfield = findViewById<TextView>(R.id.password_field)
+            Firebase.auth.signInWithEmailAndPassword( idfield.text.toString(), passfield.text.toString())
+                .addOnCompleteListener {
+                    if (it.isSuccessful) {
+                        val intent = Intent(this, UserInfoActivity::class.java)
+                        startActivity(intent)
+                    } else {
+                        println("not allow")
+                    }
+                }
         }
     }
 }
