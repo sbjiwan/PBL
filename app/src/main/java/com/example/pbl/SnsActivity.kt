cf939c310d580ab28685c73f8db069f05402ad87
hynrusang <84229112+hynrusang@users.noreply.github.com>
2022-11-24 06:05:30 +0900

    prerelease beta-0
    유저 프로필 기능에 따른, 게시글 반영

--- app/src/main/java/com/example/pbl/SnsActivity.kt
+++ app/src/main/java/com/example/pbl/SnsActivity.kt
@@ -2,14 +2,14 @@ package com.example.pbl
 
 import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
-import android.widget.ImageView
-import android.widget.LinearLayout
-import android.widget.TextView
+import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
+import com.bumptech.glide.Glide
+import com.google.android.gms.tasks.OnCompleteListener
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
+import com.google.firebase.storage.FirebaseStorage
 import org.w3c.dom.Text
 
 class SnsActivity : AppCompatActivity() {
@@ -20,9 +20,20 @@ class SnsActivity : AppCompatActivity() {
         Firebase.firestore.collection("post_list").get().addOnSuccessListener {
             for (data in it) {
                 val post = layoutInflater.inflate(R.layout.post_view, null, false);
-                post.findViewById<TextView>(R.id.username).setText(Firebase.auth.currentUser?.email.toString());
-                post.findViewById<TextView>(R.id.post_title).setText(data["postname"].toString())
-                post.findViewById<TextView>(R.id.post_main).setText(data["postmain"].toString())
+
+                val ref = FirebaseStorage.getInstance().getReference(data["author"].toString() + "_profile")
+
+                ref.downloadUrl
+                    .addOnCompleteListener(OnCompleteListener { task ->
+                        if(task.isSuccessful){
+                            Glide.with(this)
+                                .load(task.result)
+                                .into(post.findViewById(R.id.user_profile))
+                        }
+                    })
+                post.findViewById<TextView>(R.id.username).setText(data["author"].toString());
+                post.findViewById<TextView>(R.id.post_title).setText(data["post_name"].toString())
+                post.findViewById<TextView>(R.id.post_main).setText(data["post_main"].toString())
               //  post.findViewById<TextView>(R.id.post_category).setText("카테고리: ${data["postcategory"].toString()}")
                 findViewById<LinearLayout>(R.id.post_list).addView(post);
             }
