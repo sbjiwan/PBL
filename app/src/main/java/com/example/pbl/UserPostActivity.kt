78fa78052f1d1b84f1059101b8b654a0e1076b40
hynrusang <84229112+hynrusang@users.noreply.github.com>
2022-11-26 16:31:22 +0900

    update minor : delete comment
    댓글 삭제 기능 추가

--- app/src/main/java/com/example/pbl/UserPostActivity.kt
+++ app/src/main/java/com/example/pbl/UserPostActivity.kt
@@ -8,6 +8,7 @@ import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import com.bumptech.glide.Glide
 import com.google.android.gms.tasks.OnCompleteListener
+import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
@@ -60,13 +61,35 @@ class UserPostActivity : AppCompatActivity() {
                 item.findViewById<TextView>(R.id.username).text = comment["author"]
                 item.findViewById<Button>(R.id.edit).setOnClickListener {
                     if (comment["author"] == Firebase.auth.currentUser?.email.toString()) {
-                        
+
                     } else Toast.makeText(this, "해당 댓글의 작성자만 댓글을 수정할 수 있습니다.", Toast.LENGTH_LONG).show()
                 }
                 item.findViewById<Button>(R.id.del).setOnClickListener {
-                    if (comment["author"] == Firebase.auth.currentUser?.email.toString()) {
-
-                    } else Toast.makeText(this, "해당 댓글의 작성자만 댓글을 삭제할 수 있습니다.", Toast.LENGTH_LONG).show()
+                    if (author == Firebase.auth.currentUser?.email.toString() || comment["author"] == Firebase.auth.currentUser?.email.toString()) {
+                        AlertDialog.Builder(this)
+                            .setTitle("경고")
+                            .setMessage("정말로 댓글을 삭제하시겠습니까?")
+                            .setPositiveButton("네") { dialogInterface: DialogInterface, i: Int ->
+                                val postInfo = mutableMapOf<String, Any>()
+                                Firebase.firestore.collection("post_list").document(documentuid).get().addOnSuccessListener {
+                                    postInfo["author"] = it["author"] as String
+                                    postInfo["post_name"] = it["post_name"] as String
+                                    postInfo["post_main"] = it["post_main"] as String
+                                    postInfo["time"] = it["time"] as String
+                                    val newComment = it["comment"] as ArrayList<MutableMap<String, String>>
+                                    newComment.remove(mutableMapOf<String, String>(
+                                        "author" to comment["author"].toString(),
+                                        "comment" to comment["comment"].toString()
+                                    ))
+                                    postInfo["comment"] = newComment
+                                    Firebase.firestore.collection("post_list").document(intent.getStringExtra("uid")!!).update(postInfo)
+                                    finish()
+                                    startActivity(this.intent)
+                                }
+                            }
+                            .setNegativeButton("아니오") { dialogInterface: DialogInterface, i: Int -> }
+                            .show()
+                    } else Toast.makeText(this, "해당 댓글의 작성자나 게시자만 댓글을 삭제할 수 있습니다.", Toast.LENGTH_LONG).show()
                 }
                 item.findViewById<TextView>(R.id.comment_main).text = comment["comment"]
                 findViewById<LinearLayout>(R.id.comment_list).addView(item)
