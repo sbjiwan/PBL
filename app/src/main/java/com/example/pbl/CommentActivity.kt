3163367e29dcc698f33e76a25deeea5ccf1795db
hynrusang <84229112+hynrusang@users.noreply.github.com>
2022-11-27 19:12:42 +0900

    remove import
    안쓰는 import 제거 (2차)

--- app/src/main/java/com/example/pbl/CommentActivity.kt
+++ app/src/main/java/com/example/pbl/CommentActivity.kt
@@ -7,10 +7,6 @@ import androidx.appcompat.app.AppCompatActivity
 import com.bumptech.glide.Glide
 import com.example.pbl.Utils.FirebaseUtil
 import com.google.android.gms.tasks.OnCompleteListener
-import com.google.firebase.auth.ktx.auth
-import com.google.firebase.firestore.ktx.firestore
-import com.google.firebase.ktx.Firebase
-import com.google.firebase.storage.FirebaseStorage
 import kotlin.properties.Delegates
 
 class CommentActivity : AppCompatActivity() {
